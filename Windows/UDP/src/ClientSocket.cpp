7c358e083a3669820edff55a268cd05ccc037680
andywang0607 <51164333+andywang0607@users.noreply.github.com>
2020-04-02 21:08:06 +0800

    [feat] Implement server-client method in Windows environment
    Note:
    Apply UDP protocol

---
+++ Windows/UDP/src/ClientSocket.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <winsock2.h>
+#include <Ws2tcpip.h>
+#include <string>
+using namespace std;
+
+int main()
+{
+    WSADATA  WSAData;
+    SOCKET serverSocket;
+    SOCKADDR_IN addr;
+
+    WSAStartup(MAKEWORD(2, 0), &WSAData);
+    serverSocket = socket(AF_INET, SOCK_DGRAM, 0);
+
+    PCSTR src = TEXT("127.0.0.1");
+    InetPton(AF_INET, src, &addr.sin_addr.s_addr);
+
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(5555);
+    /* UDP do not need to connect */
+
+    std::cout << "please input message or [-1] to quit" << endl;
+
+
+    string buffer;
+    while (strcmp(buffer.c_str(), "-1") != 0) {
+        getline(cin, buffer);
+        // UDP apply sendto function
+        int sendOk = sendto(serverSocket, buffer.c_str(), buffer.length(), 0, (sockaddr*)&addr, sizeof(addr));
+        if (sendOk == SOCKET_ERROR)
+        {
+            cout << "That didn't work! " << WSAGetLastError() << endl;
+        }
+        else {
+            std::cout << "Message sent!" << endl;
+        }
+        if (strcmp(buffer.c_str(), "-1") == 0) {
+            closesocket(serverSocket);
+            WSACleanup();
+            std::cout << "Socket closed" << endl;
+        }
+    }
+    system("pause");
+}
---
+++ Windows/UDP/src/ServerSocket.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <winsock2.h>
+
+using namespace std;
+
+int main()
+{
+    WSADATA  WSAData;
+    SOCKET serverSocket, clientSocket;
+    SOCKADDR_IN serverAddr, clientAddr;
+
+    int clientLength = sizeof(serverAddr);
+
+    WSAStartup(MAKEWORD(2, 0), &WSAData);
+    serverSocket = socket(AF_INET, SOCK_DGRAM, 0);
+
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(5555);
+
+    bind(serverSocket, (SOCKADDR *)&serverAddr, sizeof(serverAddr));
+    listen(serverSocket, 0);
+
+    cout << "Listening for incoming connections" << endl;
+
+    char buffer[1024];
+    int clientAddrSize = sizeof(clientAddr);
+   //UDP don not neet to accept
+        cout << "Client connected!" << endl;
+        while (strcmp(buffer, "-1") != 0) {
+            memset(buffer, 0, sizeof(buffer));
+            //UDP apply recvfrom function
+            int bytesIn = recvfrom(serverSocket, buffer, sizeof(buffer), 0, (sockaddr*)&serverAddr, &clientLength);
+            if (bytesIn == SOCKET_ERROR) {
+                cout << "Error receiving from client " << WSAGetLastError() << endl;
+                continue;
+            }
+            cout << "Client says: " << buffer << endl;
+        }
+        if (strcmp(buffer, "-1") == 0) {
+            closesocket(serverSocket);
+            std::cout << "Socket closed" << endl;
+        }
+
+
+    system("pause");
+}
+
